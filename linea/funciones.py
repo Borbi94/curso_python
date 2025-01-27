007765f4d795822faa61b785152c4ecb5e6a8389
Borbi94 <a223205113@unison.mx>
2025-01-27 14:29:36 -0700

    Cree el archivo de funciones auxiliares para linea

---
+++ linea/funciones.py
@@ -0,0 +1,13 @@
+def calcular_y(x,m,b):
+
+    return m*x+b 
+
+if __name__ == "__main__":
+    x = 0
+    m = 3
+    b = 2
+    y = calcular_y(x,m,b)
+    if y ==2:
+        print ("Prueba exitosa")
+    else:
+        print("Prueba fallida")
