4235aacfa372763bebd491204bbd69ea3e8ac319
Borbi94 <a223205113@unison.mx>
2025-01-28 14:37:52 -0700

    Agregue

--- linea/app.py
+++ linea/app.py
@@ -1,7 +1,8 @@
 import funciones
+import argparse
 
-def main(m,b):
-    X = [x/10.5 for x in range (1,101,10)]
+def main(m, b):
+    X = [x/10.0 for x in range (1,101,5)]
     Y= [funciones.calcular_y(x,m,b) for x in X]
     print(X)
     print(Y)
@@ -9,4 +10,11 @@ def main(m,b):
     print(coordenadas)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calcula las coordenas de una linea recta')
+    parser.add_argument('-m', type= float, 
+    help='Pendiente de la linea', default=2.0)
+    parser.add_argument('-b', type= float, 
+    help='Interseccion em y', default=3.0)
+    args = parser.parse_args()
+    main(args.m, args.b)
     main (2,3)
--- linea/funciones.py
+++ linea/funciones.py
@@ -1,6 +1,8 @@
-def calcular_y(x,m,b):
+def calcular_y(x: float,m: float,b: float)->float:
 
     return m*x+b 
+def test_linea():
+    y = calcular_y(0.0,2.0, 3.0)
 
 if __name__ == "__main__":
     x = 0
