5bc3ff689d039ef563d7ba569a636fc0181598cd
Borbi94 <a223205113@unison.mx>
2025-02-04 14:48:04 -0700

    ahi va avanzando

--- linea/funciones.py
+++ linea/funciones.py
@@ -1,8 +1,22 @@
+import matplotlib.pyplot as plt
+
+
 def calcular_y(x: float,m: float,b: float)->float:
 
     return m*x+b 
+
 def test_linea():
     y = calcular_y(0.0,2.0, 3.0)
+    return y
+
+def grafica_linea(X: list, Y:list, m:float, b:float):
+    plt.plot(X, Y)
+    plt.title(f'Linea recta con ={m} e interseccion en y={b}')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid
+    plt.show
+
 
 if __name__ == "__main__":
     x = 0
